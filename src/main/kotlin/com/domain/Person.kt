58f63fcaba6e533bd328d0199565fd78a88a18f5
ashikka <ashikagupta28@gmail.com>
2021-03-31 17:00:37 +0530

    feat: Implemented functions

--- src/main/kotlin/com/domain/Person.kt
+++ src/main/kotlin/com/domain/Person.kt
@@ -1,3 +1,3 @@
 package com.example.springkotlin.domain
 
-data class Person(val id: Long = 1, val name: String = "", val lastName: String? = null)
+data class Person(val id: Long = 1, var name: String = "", var lastName: String? = null)
--- src/main/kotlin/com/service/PersonManangementServiceImpl.kt
+++ src/main/kotlin/com/service/PersonManangementServiceImpl.kt
@@ -4,29 +4,42 @@ import com.dao.PersonDao
 import com.dto.AddPersonRequest
 import com.dto.PersonResponse
 import com.dto.UpdatePersonRequest
+import com.example.springkotlin.domain.Person
 import com.transformer.AddPersonRequestTransformer
+import com.transformer.toPersonResponse
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class PersonManangementServiceImpl(private val personDao: PersonDao,
                                    private val addPersonRequestTransformer: AddPersonRequestTransformer) : PersonManangementService {
-    override fun findById(id: Long): PersonResponse? {
-        TODO("Not yet implemented")
-    }
 
-    override fun findAll(): List<PersonResponse> {
-        TODO("Not yet implemented")
-    }
+    override fun findById(id: Long): PersonResponse? = this.findPersonById(id).toPersonResponse()
+
+
+    override fun findAll(): List<PersonResponse> = this.personDao.findAll().map(Person::toPersonResponse)
 
     override fun save(addPersonRequest: AddPersonRequest): PersonResponse {
-        TODO("Not yet implemented")
+
+        return this.saveOrUpdate(
+                addPersonRequestTransformer.transform(addPersonRequest)
+        )
+
     }
 
     override fun update(updatePersonRequest: UpdatePersonRequest): PersonResponse {
-        TODO("Not yet implemented")
+        val person = this.findPersonById(updatePersonRequest.id) ?: throw IllegalAccessException("{$updatePersonRequest.id} not found")
+        return this.saveOrUpdate(person.apply {
+            this.name = updatePersonRequest.name
+            this.lastName = updatePersonRequest.lastName
+        })
     }
 
     override fun deleteById(id: Long) {
-        TODO("Not yet implemented")
+        this.personDao.deleteById(id)
     }
+
+    private fun findPersonById (id: Long): Person? = this.personDao.findByIdOrNull(id)
+
+    private  fun saveOrUpdate (person: Person): PersonResponse = this.personDao.save(person).toPersonResponse()
 }
